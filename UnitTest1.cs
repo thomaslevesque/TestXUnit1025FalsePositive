ea784ea9b8d5e40b9fe99aae44dbaba3a4a01579
Thomas LEVESQUE <tlevesque@infinitesquare.com>
2019-09-23 18:16:47 +0200

    Simplify test case

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,17 +5,8 @@ namespace TestXUnit1025FalsePositive
     public class UnitTest1
     {
         [Theory]
-        // Match
-        [InlineData(3, new[] { 1, 2, 3, 3, 2, 3, 1 }, new[] { 2, 3 }, new[] { 1, 2, 3, 3, 1 })]
         [InlineData(3, new[] { 1, 2, 3, 3, 2, 3, 1 }, new[] { 1, 2, 3 }, new[] { 3, 2, 3, 1 })]
-        [InlineData(3, new[] { 1, 2, 3, 3, 2, 3, 1 }, new[] { 3 }, new[] { 1, 2, 3, 3, 2, 1 })]
-        [InlineData(3, new[] { 1, 2, 3, 3, 2, 3, 1 }, new[] { 3, 3, 2 }, new[] { 1, 2, 3, 1 })]
-        [InlineData(3, new[] { 1, 2, 3, 3, 2, 3, 1 }, new[] { 3, 1 }, new[] { 1, 2, 3, 3, 2 })]
         [InlineData(3, new[] { 1, 2, 3, 3, 2, 3, 1 }, new[] { 1, 2, 3, 3, 2 }, new[] { 3, 1 })]
-        // No match
-        [InlineData(4, new[] { 1, 2, 3, 3, 2, 3, 1 }, new[] { 4 }, new[] { 1, 2, 3, 3, 2, 3, 1 })]
-        [InlineData(3, new[] { 1, 2, 3, 3, 2, 3, 1 }, new[] { 3, 3, 3 }, new[] { 1, 2, 3, 3, 2, 3, 1 })]
-        [InlineData(3, new[] { 1, 2, 3, 3, 2, 3, 1 }, new[] { 3, 2, 1 }, new[] { 1, 2, 3, 3, 2, 3, 1 })]
         public void Test(int count, int[] x, int[] y, int[] z)
         {
             // Just to suppress xUnit1026 (unused parameters)
